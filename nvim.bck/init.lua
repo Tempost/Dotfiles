9e4868592510ba2971ddcf1ea8c97a73a59f0098
cody <codyldiamond@gmail.com>
2023-11-27 07:21:54 -0600

    replace nvim config with nvChad style

---
+++ nvim.bck/init.lua
@@ -0,0 +1,55 @@
+require("plugins")
+
+vim.wo.number = true
+vim.wo.relativenumber = true
+vim.o.hlsearch = false
+vim.o.mouse = "a"
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.updatetime = 30
+vim.o.termguicolors = true
+vim.wo.signcolumn = "yes"
+vim.opt.scrolloff = 10
+vim.opt.wrap = true
+vim.opt.undofile = true
+vim.opt.pumheight = 10
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.hidden = true
+vim.opt.filetype = 'on'
+
+--Map blankline
+vim.g.indent_blankline_char = "|"
+vim.g.indent_blankline_filetype_exclude = { "help", "packer" }
+vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
+vim.g.indent_blankline_show_trailing_blankline_indent = false
+
+vim.g.gruvbox_baby_transparent_mode = 1
+
+vim.cmd.colorscheme("gruvbox-baby")
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = {
+    "typescriptreact",
+    "typescript",
+    "javascript",
+    "javascriptreact",
+    "json",
+    "html",
+    "lua",
+    "c",
+    "cpp",
+    "h",
+    "hpp",
+    "svelte"
+  },
+  callback = function(e)
+    vim.opt.tabstop = 2
+    vim.opt.shiftwidth = 2
+    vim.opt.softtabstop = 2
+  end,
+})
+
+vim.g.python3_host_prog = "/home/cody/.local/share/virtualenvs/neovim/bin/python3.10"
